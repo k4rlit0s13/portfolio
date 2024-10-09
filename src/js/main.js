23f81ba7eb6767fd21135a05ee84f3ba10a38082
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-08 23:31:55 -0500

    feat: :sparkles: design and style, files, and js command

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('boxlanguage').addEventListener('click', function () {
+    document.getElementById('language-modal').style.display = 'block';
+});
+
+document.getElementById('close-modal').addEventListener('click', function () {
+    document.getElementById('language-modal').style.display = 'none';
+});
