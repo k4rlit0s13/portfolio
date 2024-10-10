11e16086e85f3f7d9895f8513d004e06e282e648
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-09 20:48:05 -0500

    feat: :sparkles: animation index to home html working good

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,22 @@ document.getElementById('boxlanguage').addEventListener('click', function () {
 document.getElementById('close-modal').addEventListener('click', function () {
     document.getElementById('language-modal').style.display = 'none';
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const startButton = document.getElementById('startButton');
+
+    // Escuchar el evento de clic en el botón de "Start"
+    startButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Evitar la navegación instantánea
+
+        // Añadir la clase para deslizar todo el contenido hacia la izquierda
+        document.body.classList.add('slide-out');
+
+        // Esperar a que termine la animación antes de redirigir a la siguiente página
+        setTimeout(function() {
+            window.location.href = "../src/viewsEnglish/home.html"; // Redirigir tras la animación
+        }, 400); // Coincide con la duración de la animación
+    });
+});
+
+
