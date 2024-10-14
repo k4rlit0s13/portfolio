f5e15d6c73b503f3afa4acb536e45fb04395ad98
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-14 01:55:42 -0500

    feat: :lipstick: animations and cursos efects working

---
+++ src/js/home.js
@@ -0,0 +1,59 @@
+// home.js
+document.addEventListener('DOMContentLoaded', () => {
+    const cursorElement = document.createElement('img');
+    cursorElement.src = '../storage/img/rightHand.svg'; // Imagen inicial
+    cursorElement.style.position = 'absolute';
+    cursorElement.style.pointerEvents = 'none';
+    cursorElement.style.width = '400px'; // Ajusta el tamaño de la imagen
+    cursorElement.style.height = '400px'; // Ajusta el tamaño de la imagen
+    cursorElement.style.transform = 'translate(-120px, -160px)'; // Ajusta la posición para centrar el clic
+    document.body.appendChild(cursorElement);
+
+    // Cargar la posición del cursor desde localStorage
+    const storedX = localStorage.getItem('cursorX');
+    const storedY = localStorage.getItem('cursorY');
+    
+    if (storedX && storedY) {
+        cursorElement.style.left = `${storedX}px`;
+        cursorElement.style.top = `${storedY}px`;
+    }
+
+    function updateCursorVisibility() {
+        if (window.innerWidth > 1040) {
+            cursorElement.style.display = 'block'; // Mostrar cursor en pantallas grandes
+            document.body.style.cursor = 'none'; // Ocultar cursor por defecto
+        } else {
+            cursorElement.style.display = 'none'; // Ocultar cursor en pantallas pequeñas
+            document.body.style.cursor = 'default'; // Asegúrate de que el cursor sea el predeterminado
+        }
+    }
+
+    updateCursorVisibility();
+
+    document.addEventListener('mousemove', (event) => {
+        cursorElement.style.left = `${event.clientX}px`;
+        cursorElement.style.top = `${event.clientY}px`;
+
+        // Almacenar la posición actual en localStorage
+        localStorage.setItem('cursorX', event.clientX);
+        localStorage.setItem('cursorY', event.clientY);
+    });
+
+    window.addEventListener('resize', updateCursorVisibility);
+
+    // Almacena el ID del temporizador
+    let timeoutId;
+
+    document.addEventListener('mousedown', () => {
+        // Cambiar a la imagen de "presionado"
+        cursorElement.src = '../storage/img/clickHand.svg';
+    });
+
+    document.addEventListener('mouseup', () => {
+        // Restablecer la imagen después de un breve período
+        clearTimeout(timeoutId); // Limpiar cualquier temporizador anterior
+        timeoutId = setTimeout(() => {
+            cursorElement.src = '../storage/img/rightHand.svg'; // Volver a la imagen original después de 200 ms
+        }, 200);
+    });
+});
