b3c8eef3253c0f7533beb4f4f7e03150d5459e63
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-24 15:22:19 -0500

    feat: :lipstick: animations home fixed and changed

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cursorElement = document.createElement('img');
+    cursorElement.src = './src/storage/img/handpointselect.svg'; // Imagen inicial
+    cursorElement.style.position = 'absolute';
+    cursorElement.style.pointerEvents = 'none';
+    cursorElement.style.width = '350px'; // Ajusta el tamaño de la imagen
+    cursorElement.style.height = '350px'; // Ajusta el tamaño de la imagen
+    cursorElement.style.transform = 'translate(-30px, -60px)'; // Ajusta la posición para centrar el clic
+    cursorElement.style.zIndex = '10'; // Z-index moderado para no afectar otros elementos
+    document.body.appendChild(cursorElement);
+
+    // Función para actualizar la visibilidad del cursor
+    function updateCursorVisibility() {
+        if (window.innerWidth > 1040) {
+            cursorElement.style.display = 'block'; // Mostrar cursor en pantallas grandes
+            document.body.style.cursor = 'none'; // Ocultar cursor por defecto
+        } else {
+            cursorElement.style.display = 'none'; // Ocultar cursor en pantallas pequeñas
+            document.body.style.cursor = 'default'; // Mostrar cursor por defecto
+        }
+    }
+
+    updateCursorVisibility();
+
+    // Mover el cursor con el mouse
+    document.addEventListener('mousemove', (event) => {
+        cursorElement.style.left = `${event.clientX}px`;
+        cursorElement.style.top = `${event.clientY}px`;
+    });
+
+    // Actualizar visibilidad al redimensionar la ventana
+    window.addEventListener('resize', updateCursorVisibility);
+
+    // Cambiar imagen al hacer clic y restaurarla
+    document.addEventListener('mousedown', () => {
+        cursorElement.src = './src/storage/img/handpointselectclick.svg'; // Imagen de "presionado"
+    });
+    document.addEventListener('mouseup', () => {
+        setTimeout(() => {
+            cursorElement.src = './src/storage/img/handpointselect.svg'; // Restaurar la imagen original
+        }, 100);
+    });
+});
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const boxLanguage = document.getElementById('boxlanguage');
     const languageModal = document.getElementById('language-modal');
